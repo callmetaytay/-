3062fcec986a0fdcc8b4bb0a9602210f5900859f
callmetaytay <145259471+callmetaytay@users.noreply.github.com>
2023-12-23 17:33:01 +0800

    Add files via upload

---
+++ 归并排序.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+int n;
+const int N = 1e6 + 10;
+int q[N], temp[N];
+void merge_sort(int q[], int l, int r) {
+    if (l >= r)return;
+    int mid = l + r >> 1;
+    merge_sort(q, l, mid);
+    merge_sort(q, mid + 1, r);//递归左右两边
+    int i = l, j = mid + 1;
+    int k = 0;
+    while (i <= mid && j <= r) {
+        if (q[i] <= q[j])temp[k++] = q[i++];
+        else temp[k++] = q[j++];
+    }
+    while (i <= mid)temp[k++] = q[i++];
+    while (j <= r)temp[k++] = q[j++];
+    for (i = l, j = 0; i <= r; i++, j++)q[i] = temp[j];//把temp中的数移回到q中
+}
---
+++ 快速排序.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+void quick_sort(int q[], int l, int r) {
+    if (l >= r)return;
+    int x = q[(l + r) / 2], i = l - 1, j = r + 1;//x可以取任意值，但是取（l+r）/2时可以缩短程序运行时间
+    while (i < j) {
+        do i++; while (q[i] < x);
+        do j--; while (q[j] > x);
+        if (i < j)swap(q[i], q[j]);
+    }
+    //递归
+    quick_sort(q, l, j);//j 是小于等于中间值 x 的最右边位置，为保证左区间的数均小于等于x，所以将j作为左区间的右边界；j+1作为右区间的左边界
+    quick_sort(q, j + 1, r);//同理也可以是(q,l,i-1),(q,i,r)
+}
